f8aa6a57ac8ace86815c4e05fc43a8d8edcc9811
Ignacio Lorenzo Vélez <i.lorenzo@preving.com>
2020-11-20 11:34:30 +0100

    20/11/2020

--- GestorExamenes/application/views/instalacion.php
+++ GestorExamenes/application/views/instalacion.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php
-include('C:\xampp\htdocs\GestorExamenes\application\views\templates\head.php');
+include('templates/head.php');
 ?>
 <body id="instalacion" class="container-fluid">
 <div >
-    <form method="post" action="http://localhost/GestorExamenes/Instalacion/crearArchivo">
+    <form method="post" action=<?php echo base_url() ?>Instalacion/crearArchivo>
         <div class="form-group">
             <label for="NombreBD">Nombre de la base de datos</label>
             <input name="nombrebd" required type="text" class="form-control" id="NombreBD" placeholder="Nombre de la base de datos">
